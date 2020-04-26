c555775747a0ba4d4b0afbd8bb09dd900dd4628f
Sergiy Tolkachov <metatypeman@gmail.com>
2020-04-26 18:04:26 +0300

    snapshot

---
+++ SymOntoClayAsset/Assets/SymOntoClay/Game.cs
@@ -0,0 +1,20 @@
+﻿using SymOntoClay.UnityAsset.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SymOntoClay.Unity3D
+{
+    public class Game : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        protected virtual void Start()
+        {
+            Debug.Log("Start");
+
+            GameCore.SetSettings();
+
+            Debug.Log("Start (2)");
+        }
+    }
+}
