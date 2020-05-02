6927c36a791e23fec3d1a70cea161d8d745c9f5a
Sergiy Tolkachov <metatypeman@gmail.com>
2020-05-02 18:53:08 +0300

    snapshot

---
+++ SymOntoClayAsset/Assets/SymOntoClay/World.cs
@@ -0,0 +1,26 @@
+﻿using SymOntoClay.UnityAsset.Core;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace SymOntoClay.Unity3D
+{
+    public class World : MonoBehaviour
+    {
+        protected virtual void Awake()
+        {
+            Debug.Log($"Awake {Directory.GetCurrentDirectory()}");
+        }
+
+        // Start is called before the first frame update
+        protected virtual void Start()
+        {
+            Debug.Log("Start");
+
+            //WorldCore.SetSettings();
+
+            Debug.Log("Start (2)");
+        }
+    }
+}
