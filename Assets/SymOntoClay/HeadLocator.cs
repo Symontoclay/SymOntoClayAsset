abaa09dd54ac65984414a3beb5ce4021dfd1f604
metatypeman <metatypeman@gmail.com>
2021-08-07 12:37:16 +0300

    HeadLocator. C# documentation in progress....

---
+++ Assets/SymOntoClay/HeadLocator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SymOntoClay
+{
+    /// <summary>
+    /// Head locator.
+    /// It allows us to detect head for scanner of visible object without explicit linking Head to HumanoidNPC.
+    /// </summary>
+    [AddComponentMenu("SymOntoClay/HeadLocator")]
+    public class HeadLocator : MonoBehaviour
+    {
+    }
+}
