93ca9f1beae3798f9d8795d59ef5645e5a1c59d9
metatypeman <metatypeman@gmail.com>
2021-10-04 19:49:24 +0300

    0.3.4 snapshot

---
+++ Assets/SymOntoClay/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SymOntoClay
+{
+    [AddComponentMenu("SymOntoClay/Player")]
+    public class Player : MonoBehaviour
+    {
+    }
+}
