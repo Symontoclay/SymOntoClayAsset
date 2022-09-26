769b6d4318863ee165a4d8f03df624982a6a7836
metatypeman <metatypeman@gmail.com>
2022-09-26 20:39:07 +0300

    0.4.2 snapshot

--- Assets/SymOntoClay/Editors/LinkingWaypointEditor.cs
+++ Assets/SymOntoClay/Editors/LinkingWaypointEditor.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using SymOntoClay.UnityAsset.Components;
+using SymOntoClay.UnityAsset.Core;
+using SymOntoClay.UnityAsset.Helpers;
+using SymOntoClay.UnityAsset.Navigation;
+using SymOntoClay.UnityAsset.Scriptables;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,8 +16,40 @@ using UnityEngine;
 namespace SymOntoClay.UnityAsset.Editors
 {
 #if UNITY_EDITOR
-    public class LinkingWaypointEditor
+    [CustomEditor(typeof(LinkingWaypoint))]
+    [CanEditMultipleObjects]
+    public class LinkingWaypointEditor : Editor
     {
+        private LinkingWaypoint _target;
+
+        private void OnEnable()
+        {
+            _target = (LinkingWaypoint)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            GUILayout.BeginVertical();
+
+            _target.SobjFile = (SobjFile)EditorGUILayout.ObjectField("App File", _target.SobjFile, typeof(SobjFile), false);
+
+            var newIdValue = EditorGUILayout.TextField("Id", _target.Id);
+
+            if (_target.Id != newIdValue && EditorHelper.IsValidId(newIdValue))
+            {
+                UniqueIdRegistry.RemoveId(_target.Id);
+                UniqueIdRegistry.AddId(newIdValue);
+
+                _target.Id = newIdValue;
+            }
+
+            GUILayout.EndVertical();
+
+            if (GUI.changed)
+            {
+                EditorUtility.SetDirty(_target);
+            }
+        }
     }
 #endif
 }
