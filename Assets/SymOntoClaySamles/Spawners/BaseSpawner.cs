3307e29c48b7f17387adf8e509adbdb03a1c24c0
metatypeman <metatypeman@gmail.com>
2023-01-14 21:59:18 +0200

    0.4.3 greed spawner

---
+++ Assets/SymOntoClaySamles/Spawners/BaseSpawner.cs
@@ -0,0 +1,43 @@
+﻿using SymOntoClay.UnityAsset.Components;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.SymOntoClaySamles.Spawners
+{
+    public class BaseSpawner : MonoBehaviour
+    {
+        public GameObject Prefab;
+        public List<GameObject> PropsToSpawn;
+
+        protected void ProcessInstantiate(Vector3 position)
+        {
+            var instance = Instantiate(Prefab, position, transform.rotation);
+
+            var humanoidNPC = instance.GetComponent<HumanoidNPC>();
+
+#if DEBUG
+            Debug.Log($"BaseSpawner Start humanoidNPC == null = {humanoidNPC == null}");
+#endif
+
+#if DEBUG
+            Debug.Log($"BaseSpawner Start PropsToSpawn?.Count = {PropsToSpawn?.Count}");
+#endif
+
+            if (PropsToSpawn.Any())
+            {
+                foreach (var propItem in PropsToSpawn)
+                {
+                    var propInstance = Instantiate(propItem, position, transform.rotation);
+
+                    var handThing = propInstance.GetComponent<HandThing>();
+
+#if DEBUG
+                    Debug.Log($"BaseSpawner Start handThing == null = {handThing == null}");
+#endif
+                }
+            }
+        }
+    }
+}
--- Assets/SymOntoClaySamles/Spawners/SimpleSpawner.cs
+++ Assets/SymOntoClaySamles/Spawners/SimpleSpawner.cs
@@ -1,4 +1,5 @@
-﻿using SymOntoClay.UnityAsset.Components;
+﻿using Assets.SymOntoClaySamles.Spawners;
+using SymOntoClay.UnityAsset.Components;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,45 +8,12 @@ using UnityEngine;
 namespace SymOntoClay.UnityAsset.Samles.Spawners
 {
     [AddComponentMenu("SymOntoClay Samles/SimpleSpawner")]
-    public class SimpleSpawner : MonoBehaviour
+    public class SimpleSpawner : BaseSpawner
     {
-        public GameObject Prefab;
-        public List<GameObject> PropsToSpawn;
-
         // Use this for initialization
         void Start()
         {
-            var instance = Instantiate(Prefab, transform.position, transform.rotation);
-
-            var humanoidNPC = instance.GetComponent<HumanoidNPC>();
-
-#if DEBUG
-            Debug.Log($"SimpleSpawner Start humanoidNPC == null = {humanoidNPC == null}");
-#endif
-
-#if DEBUG
-            Debug.Log($"SimpleSpawner Start PropsToSpawn?.Count = {PropsToSpawn?.Count}");
-#endif
-
-            if(PropsToSpawn.Any())
-            {
-                foreach(var propItem in PropsToSpawn)
-                {
-                    var propInstance = Instantiate(propItem, transform.position, transform.rotation);
-
-                    var handThing = propInstance.GetComponent<HandThing>();
-
-#if DEBUG
-                    Debug.Log($"SimpleSpawner Start handThing == null = {handThing == null}");
-#endif
-                }
-            }
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
+            ProcessInstantiate(transform.position);
         }
     }
 }
